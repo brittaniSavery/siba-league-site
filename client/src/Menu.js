f4b990a462135581985803bd52b0740e6b055575
Brittani S Avery <brittani.s.avery@gmail.com>
2018-10-06 18:24:19 -0500

    Fill in menu with link names

--- client/src/Menu.js
+++ client/src/Menu.js
@@ -1,32 +1,38 @@
 import React, { Component } from 'react';
-import {Nav,Navbar,NavItem,NavDropdown,MenuItem} from 'react-bootstrap'
+import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
 import './App.css';
 
 class Menu extends Component {
-  render() {
-    return (
-        <Navbar>
-        <Navbar.Header>
-            <Navbar.Brand>
-                <a href="#home">SIBA</a>
-            </Navbar.Brand>
-        </Navbar.Header>
-        <Nav>
-            <NavItem eventKey={1} href="#">
-                Link
-                </NavItem>
-            <NavItem eventKey={2} href="#">
-                Link
-                </NavItem>
-            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                <MenuItem eventKey={3.1}>Action</MenuItem>
-                <MenuItem eventKey={3.2}>Another action</MenuItem>
-                <MenuItem eventKey={3.3}>Something else here</MenuItem>
-            </NavDropdown>
-        </Nav>
-    </Navbar>
-    );
-  }
+    render() {
+        return (
+            <Navbar>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#home">[img here]</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavDropdown eventKey={1} title="SIBA">
+                            <MenuItem eventKey={1.1}>League Standings</MenuItem>
+                            <MenuItem eventKey={1.2}>League Leaders</MenuItem>
+                        </NavDropdown>
+                        <NavDropdown eventKey={2} title="DBL">
+                            <MenuItem eventKey={2.1}>League Standings</MenuItem>
+                            <MenuItem eventKey={2.2}>League Leaders</MenuItem>
+                        </NavDropdown>
+                        <NavItem eventKey={3} href="#">Transactions</NavItem>
+                        <NavDropdown eventKey={4} title="Free Agents">
+                            <MenuItem eventKey={4.1}>Players</MenuItem>
+                            <MenuItem eventKey={4.2}>Coaches</MenuItem>
+                        </NavDropdown>
+                        <NavItem eventKey={5} href="#">Upload</NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
 }
 
 export default Menu;
