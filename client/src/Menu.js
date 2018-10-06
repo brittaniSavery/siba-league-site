57eec1d67e64af6857d4ff5604eae995ed8c20fb
Brittani S Avery <brittani.s.avery@gmail.com>
2018-10-06 18:52:35 -0500

    Add Information on menu

--- client/src/Menu.js
+++ client/src/Menu.js
@@ -14,20 +14,27 @@ class Menu extends Component {
                 </Navbar.Header>
                 <Navbar.Collapse>
                     <Nav>
-                        <NavDropdown eventKey={1} title="SIBA">
-                            <MenuItem eventKey={1.1}>League Standings</MenuItem>
-                            <MenuItem eventKey={1.2}>League Leaders</MenuItem>
+                        <NavDropdown eventKey={1} title="Information">
+                            <MenuItem eventKey={1.1}>Creators</MenuItem>
+                            <MenuItem eventKey={1.2}>Rules</MenuItem>
+                            <MenuItem eventKey={1.3}>Rewards</MenuItem>
+                            <MenuItem eventKey={1.3}>Join</MenuItem>
                         </NavDropdown>
-                        <NavDropdown eventKey={2} title="DBL">
+                        <NavDropdown eventKey={2} title="SIBA">
                             <MenuItem eventKey={2.1}>League Standings</MenuItem>
                             <MenuItem eventKey={2.2}>League Leaders</MenuItem>
+                            <MenuItem eventKey={2.2}>Owners</MenuItem>
                         </NavDropdown>
-                        <NavItem eventKey={3} href="#">Transactions</NavItem>
-                        <NavDropdown eventKey={4} title="Free Agents">
-                            <MenuItem eventKey={4.1}>Players</MenuItem>
-                            <MenuItem eventKey={4.2}>Coaches</MenuItem>
+                        <NavDropdown eventKey={3} title="DBL">
+                            <MenuItem eventKey={3.1}>League Standings</MenuItem>
+                            <MenuItem eventKey={3.2}>League Leaders</MenuItem>
                         </NavDropdown>
-                        <NavItem eventKey={5} href="#">Upload</NavItem>
+                        <NavItem eventKey={4} href="#">Transactions</NavItem>
+                        <NavDropdown eventKey={5} title="Free Agents">
+                            <MenuItem eventKey={5.1}>Players</MenuItem>
+                            <MenuItem eventKey={54.2}>Coaches</MenuItem>
+                        </NavDropdown>
+                        <NavItem eventKey={6} href="#">Upload</NavItem>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
