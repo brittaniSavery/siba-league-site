b0bdc348b278ab6fc511cf44d59780dac152a7e1
Brittani S Avery <brittani.s.avery@gmail.com>
2018-10-15 21:23:44 -0500

    Add basic setup for Upload page

--- client/src/Upload.js
+++ client/src/Upload.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import { FieldGroup } from 'react-bootstrap';
+import { FieldGroup } from './Utilities.js';
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 const Upload = () => (
     <section>
-        <h2>Upload</h2>
+        <header>
+            <h2>Upload</h2>
+        </header>
+
+        <p>Using the form below, select a valid DDSPB team save file, league password, team password.</p>
+
+        <form>
+            <FieldGroup
+                id="teamSaveFile"
+                type="file"
+                label="Select Your Team File:"
+            />
+
+            <FieldGroup 
+                id="leaguePassword"
+                label="League Password:"
+                type="password"
+            />
+            
+            <FieldGroup 
+                id="teamPassword"
+                label="Team Password:"
+                type="password"
+            />
+
+            <Button type="submit">Upload</Button>
+        </form>
+
     </section>
 );
 
