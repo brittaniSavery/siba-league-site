a909be1dfee553c73aa72e37ead44e6e64f34705
Brittani S Avery <brittani.s.avery@gmail.com>
2019-01-27 21:56:50 -0600

    Add footer for copyright

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Menu from './Menu.js';
+import Footer from './Footer.jsx'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBasketballBall } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
@@ -12,6 +13,7 @@ const App = () => (
           <h2>Simulation International Basketball Assoication</h2>
         </header>
         <Menu />
+        <Footer />
       </main>
     );
 
---
+++ client/src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+import * as copyRange from 'rainge'
+
+const Footer = () => (
+    <Navbar fixedBottom={true}>
+        <Navbar.Text pullRight>&copy; {copyRange(2018)} Avery Incorporated</Navbar.Text>
+    </Navbar>
+);
+
+export default Footer;
