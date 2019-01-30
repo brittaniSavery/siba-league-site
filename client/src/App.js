8a7e3c5315db19d76ad21e6313b7923cd39480c2
Brittani S Avery <brittani.s.avery@gmail.com>
2019-01-29 22:59:33 -0600

    Add temp logo

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Menu from './Menu.js';
 import Footer from './Footer.jsx'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBasketballBall } from '@fortawesome/free-solid-svg-icons';
+import sibaLogo from './siba-logo.svg'
 import './App.css';
 
 library.add(faBasketballBall)
@@ -10,6 +11,7 @@ library.add(faBasketballBall)
 const App = () => (
       <main>
         <header className="header" id="siteHeader">
+        <img src={sibaLogo} alt="SIBA Logo" />
           <h1>Simulation International Basketball Assoication</h1>
         </header>
         <Menu />
