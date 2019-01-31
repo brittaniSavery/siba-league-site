dd8cd5bb253bcaeacfd20fac68282e8ef10066c0
Brittani S Avery <brittani.s.avery@gmail.com>
2019-01-30 22:13:23 -0600

    Add new logo and mobile-friendliness

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,49 @@ import Menu from './Menu.js';
 import Footer from './Footer.jsx'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBasketballBall } from '@fortawesome/free-solid-svg-icons';
-import sibaLogo from './siba-logo.svg'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import * as DisplayPages from './DisplayPages.jsx';
+import Upload from './Upload.js';
+import Join from './Join.js';
+import sibaLogo from './temp-logo.svg'
+import sibaLogoMini from './temp-logo-mini.svg'
 import './App.css';
 
 library.add(faBasketballBall)
 
 const App = () => (
-      <main>
-        <header className="header" id="siteHeader">
-        <img src={sibaLogo} alt="SIBA Logo" />
+  <Router basename="/newsite">
+    <main>
+      <header id="siteHeader">
+        <div className="center hidden-xs">
+          <img src={sibaLogo} alt="SIBA Logo" />
           <h1>Simulation International Basketball Assoication</h1>
-        </header>
-        <Menu />
-        <Footer />
-      </main>
-    );
+        </div>
+        <div className="center visible-xs-block hidden-sm hidden-md hidden-lg">
+          <img src={sibaLogoMini} alt="SIBA Logo" />
+          <h1>SIBA</h1>
+        </div>
+      </header>
+      <Menu />
+      <section id="content">
+        <Route exact path="/" component={DisplayPages.Home} />
+        <Route path="/info/about" component={DisplayPages.About} />
+        <Route path="/info/rules" component={DisplayPages.Rules} />
+        <Route path="/info/rewards" component={DisplayPages.Rewards} />
+        <Route path="/info/join" component={Join} />
+        <Route path="/siba/standings" component={DisplayPages.SibaStandings} />
+        <Route path="/siba/leaders" component={DisplayPages.SibaLeaders} />
+        <Route path="/siba/owners" component={DisplayPages.Owners} />
+        <Route path="/dbl/standings" component={DisplayPages.DblStandings} />
+        <Route path="/dbl/leaders" component={DisplayPages.DblLeaders} />
+        <Route path="/transactions" component={DisplayPages.Transactions} />
+        <Route path="/fa/players" component={DisplayPages.FaPlayers} />
+        <Route path="/fa/coaches" component={DisplayPages.FaCoaches} />
+        <Route path="/upload" component={Upload} />
+      </section>
+      <Footer />
+    </main>
+  </Router>
+);
 
 export default App;
--- client/src/Menu.js
+++ client/src/Menu.js
@@ -1,92 +1,71 @@
 import React from 'react';
 import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import  * as DisplayPages from './DisplayPages.jsx';
-import Upload from './Upload.js';
-import Join from './Join.js';
 import './App.css';
 
 const Menu = () => (
-    <Router basename="/newsite">
-        <div>
-            <Navbar>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <Link to="/"><FontAwesomeIcon icon="basketball-ball"/></Link>
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                </Navbar.Header>
-                <Navbar.Collapse>
-                    <Nav>
-                        <NavDropdown eventKey={1} title="Information" id="info">
-                            <LinkContainer to="/info/about">
-                                <MenuItem eventKey={1.1}>About</MenuItem>
-                            </LinkContainer>
-                            <LinkContainer to="/info/rules">
-                                <MenuItem eventKey={1.2}>Rules</MenuItem>
-                            </LinkContainer>
-                            <LinkContainer to="/info/rewards">
-                                <MenuItem eventKey={1.3}>Rewards</MenuItem>
-                            </LinkContainer>
-                            <LinkContainer to="/info/join">
-                                <MenuItem eventKey={1.4}>Join</MenuItem>
-                            </LinkContainer>
-                        </NavDropdown>
-                        <NavDropdown eventKey={2} title="SIBA" id="siba">
-                            <LinkContainer to="/siba/standings">
-                                <MenuItem eventKey={2.1}>League Standings</MenuItem>
-                            </LinkContainer>
-                            <LinkContainer to="/siba/leaders">
-                                <MenuItem eventKey={2.2}>League Leaders</MenuItem>
-                            </LinkContainer>
-                            <LinkContainer to="/siba/owners">
-                                <MenuItem eventKey={2.3}>Owners</MenuItem>
-                            </LinkContainer>
-                        </NavDropdown>
-                        <NavDropdown eventKey={3} title="DBL" id="dbl">
-                            <LinkContainer to="/dbl/standings">
-                                <MenuItem eventKey={3.1}>League Standings</MenuItem>
-                            </LinkContainer>
-                            <LinkContainer to="/dbl/leaders">
-                                <MenuItem eventKey={3.2}>League Leaders</MenuItem>
-                            </LinkContainer>
-                        </NavDropdown>
-                        <LinkContainer to="/transactions">
-                            <NavItem eventKey={4}>Transactions</NavItem>
-                        </LinkContainer>
-                        <NavDropdown eventKey={5} title="Free Agents" id="fa">
-                            <LinkContainer to="/fa/players">
-                                <MenuItem eventKey={5.1}>Players</MenuItem>
-                            </LinkContainer>
-                            <LinkContainer to="/fa/coaches">
-                                <MenuItem eventKey={5.2}>Coaches</MenuItem>
-                            </LinkContainer>
-                        </NavDropdown>
-                        <LinkContainer to="/upload">
-                            <NavItem eventKey={6}>Upload</NavItem>
-                        </LinkContainer>
-                        
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-            <Route exact path="/" component={DisplayPages.Home} />
-            <Route path="/info/about" component={DisplayPages.About} />
-            <Route path="/info/rules" component={DisplayPages.Rules} />
-            <Route path="/info/rewards" component={DisplayPages.Rewards} />
-            <Route path="/info/join" component={Join} />
-            <Route path="/siba/standings" component={DisplayPages.SibaStandings} />
-            <Route path="/siba/leaders" component={DisplayPages.SibaLeaders} />
-            <Route path="/siba/owners" component={DisplayPages.Owners} />
-            <Route path="/dbl/standings" component={DisplayPages.DblStandings} />
-            <Route path="/dbl/leaders" component={DisplayPages.DblLeaders} />
-            <Route path="/transactions" component={DisplayPages.Transactions} />
-            <Route path="/fa/players" component={DisplayPages.FaPlayers} />
-            <Route path="/fa/coaches" component={DisplayPages.FaCoaches} />
-            <Route path="/upload" component={Upload} />
-        </div>
-    </Router>
+    <Navbar>
+        <Navbar.Header>
+            <Navbar.Brand>
+                <Link to="/"><FontAwesomeIcon icon="basketball-ball"/></Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+            <Nav>
+                <NavDropdown eventKey={1} title="Information" id="info">
+                    <LinkContainer to="/info/about">
+                        <MenuItem eventKey={1.1}>About</MenuItem>
+                    </LinkContainer>
+                    <LinkContainer to="/info/rules">
+                        <MenuItem eventKey={1.2}>Rules</MenuItem>
+                    </LinkContainer>
+                    <LinkContainer to="/info/rewards">
+                        <MenuItem eventKey={1.3}>Rewards</MenuItem>
+                    </LinkContainer>
+                    <LinkContainer to="/info/join">
+                        <MenuItem eventKey={1.4}>Join</MenuItem>
+                    </LinkContainer>
+                </NavDropdown>
+                <NavDropdown eventKey={2} title="SIBA" id="siba">
+                    <LinkContainer to="/siba/standings">
+                        <MenuItem eventKey={2.1}>League Standings</MenuItem>
+                    </LinkContainer>
+                    <LinkContainer to="/siba/leaders">
+                        <MenuItem eventKey={2.2}>League Leaders</MenuItem>
+                    </LinkContainer>
+                    <LinkContainer to="/siba/owners">
+                        <MenuItem eventKey={2.3}>Owners</MenuItem>
+                    </LinkContainer>
+                </NavDropdown>
+                <NavDropdown eventKey={3} title="DBL" id="dbl">
+                    <LinkContainer to="/dbl/standings">
+                        <MenuItem eventKey={3.1}>League Standings</MenuItem>
+                    </LinkContainer>
+                    <LinkContainer to="/dbl/leaders">
+                        <MenuItem eventKey={3.2}>League Leaders</MenuItem>
+                    </LinkContainer>
+                </NavDropdown>
+                <LinkContainer to="/transactions">
+                    <NavItem eventKey={4}>Transactions</NavItem>
+                </LinkContainer>
+                <NavDropdown eventKey={5} title="Free Agents" id="fa">
+                    <LinkContainer to="/fa/players">
+                        <MenuItem eventKey={5.1}>Players</MenuItem>
+                    </LinkContainer>
+                    <LinkContainer to="/fa/coaches">
+                        <MenuItem eventKey={5.2}>Coaches</MenuItem>
+                    </LinkContainer>
+                </NavDropdown>
+                <LinkContainer to="/upload">
+                    <NavItem eventKey={6}>Upload</NavItem>
+                </LinkContainer>
+                
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
 );
 
 export default Menu;
