a7bbc95553c71c2a23b68e8b898d919abd0b0c75
Brittani S Avery <brittani.s.avery@gmail.com>
2019-02-05 22:37:43 -0600

    Add state for form changes

--- client/src/Join.js
+++ client/src/Join.js
@@ -2,47 +2,79 @@ import React from 'react';
 import { FieldGroup, BasicHeader } from './Utilities.js';
 import { Button, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 import './App.css';
+class Join extends React.PureComponent {
 
-const Join = () => (
-    <section className="container">
-        <BasicHeader title="Join" />
-        <p>Intersting in joining the SIBA as the general manager of your own basketball team? Fill out the form below and the commissioner
-            will contact you with more information on the available teams and other important steps.
-        </p>
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            foundBy: '',
+            reason: ''
+        };
+    }
 
-        <form>
-            <FieldGroup
-                id="name"
-                type="text"
-                label="Name:"
-            />
+    handleSubmit = () => {
+        
+    }
 
-            <FieldGroup 
-                id="email"
-                type="email"
-                label="Email:"
-            />
+    handleOnChange = (event) => {
+        this.setState({[event.target.id]: event.target.value});
+    }
 
-            <FormGroup controlId="formControlsSelect">
-            <ControlLabel>Found SIBA by:</ControlLabel>
-            <FormControl componentClass="select" placeholder="select">
-                <option value="referral">Friend/Family</option>
-                <option value="google">Google</option>
-                <option value="fb">Facebook</option>
-                <option value="twitter">Twitter</option>
-                <option value="other">Other</option>
-            </FormControl>
-            </FormGroup>
+    render() {
+        return (
+            <section className="container">
+                <BasicHeader title="Join" />
 
-            <FormGroup controlId="join-reason">
-                <ControlLabel>Reason for Joining:</ControlLabel>
-                <FormControl componentClass="textarea" placeholder="Optional" />
-            </FormGroup>
-
-            <Button type="submit">Submit</Button>
-        </form>
-
-    </section>
-);
+                <p>Intersting in joining the SIBA as the general manager of your own basketball team? Fill out the form below and the commissioner
+                will contact you with more information on the available teams and other important steps.
+                </p>
+                
+                <form>
+                    <FieldGroup
+                    id="name"
+                    type="text"
+                    label="Name:"
+                    value={this.state.name}
+                    onChange={e => this.handleOnChange(e)}
+                    />
+                    
+                    <FieldGroup 
+                    id="email"
+                    type="email"
+                    label="Email:"
+                    value={this.state.email}
+                    onChange={e => this.handleOnChange(e)}
+                    />
+                    
+                    <FormGroup controlId="foundBy">
+                    <ControlLabel>Found SIBA by:</ControlLabel>
+                        <FormControl 
+                        onChange={e => this.handleOnChange(e)} 
+                        componentClass="select" placeholder="Choose one">
+                            <option value="">Choose one</option>
+                            <option value="referral">Friend/Family</option>
+                            <option value="google">Google</option>
+                            <option value="fb">Facebook</option>
+                            <option value="twitter">Twitter</option>
+                            <option value="other">Other</option>
+                        </FormControl>
+                    </FormGroup>
+                    
+                    <FormGroup controlId="reason">
+                        <ControlLabel>Reason for Joining:</ControlLabel>
+                        <FormControl 
+                        componentClass="textarea" placeholder="Optional" 
+                        value={this.state.reason} 
+                        onChange={e => this.handleOnChange(e)} />
+                    </FormGroup>
+                    
+                    <Button type="submit" onClick={this.handleSubmit()}>Submit</Button>
+                </form>
+            </section>
+        );
+    }
+}
 
 export default Join;
