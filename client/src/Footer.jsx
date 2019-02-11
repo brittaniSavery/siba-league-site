8e80f2e027acf76e897d96910beafac2d4cdaa74
Brittani S Avery <brittani.s.avery@gmail.com>
2019-02-10 18:21:52 -0600

    Add free icons credit

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -4,7 +4,10 @@ import * as copyRange from 'rainge'
 
 const Footer = () => (
     <Navbar fixedBottom={true}>
-        <Navbar.Text pullRight>&copy; {copyRange(2018)} Avery Incorporated</Navbar.Text>
+        <Navbar.Text pullRight>
+            <p>&copy; {copyRange(2018)} Avery Incorporated<br />
+            Icons/Vectors by <a href="https://icons8.com/material-icons">Icons8</a> & <a target="_blank" href="https://www.vecteezy.com/">Vecteezy.com</a></p>
+        </Navbar.Text>
     </Navbar>
 );
 
