5e4eae9de07e6b28985f558c3f060baf28d78afe
Brittani S Avery <brittani.s.avery@gmail.com>
2020-03-18 00:58:13 -0500

    Add team info and images

---
+++ siba/src/utilities/Teams.js
@@ -0,0 +1,160 @@
+export const atlanta = {
+  name: "Atlanta Generals",
+  logo: "../images/teams/atlantasiba.png",
+  alt: "Atlanta Generals Team Logo"
+};
+export const baltimore = {
+  name: "Baltimore Outlaws",
+  logo: "../images/teams/baltimoresiba.png",
+  alt: "Baltimore Outlaws Team Logo"
+};
+export const boston = {
+  name: "Boston Nighthawks",
+  logo: "../images/teams/bostonsiba.png",
+  alt: "Boston Nighthawks Team Logo"
+};
+export const calgary = {
+  name: "Calgary Charge",
+  logo: "../images/teams/calgarysiba.png",
+  alt: "Calgary Charge Team Logo"
+};
+export const charlotte = {
+  name: "Charlotte Wasps",
+  logo: "../images/teams/charlottesiba.png",
+  alt: "Charlotte Wasps Team Logo"
+};
+export const chicago = {
+  name: "Chicago Guardians",
+  logo: "../images/teams/chicagosiba.png",
+  alt: "Chicago Guardians Team Logo"
+};
+export const columbus = {
+  name: "Columbus Barons",
+  logo: "../images/teams/columbussiba.png",
+  alt: "Columbus Barons Team Logo"
+};
+export const dallas = {
+  name: "Dallas Rangers",
+  logo: "../images/teams/dallassiba.png",
+  alt: "Dallas Rangers Team Logo"
+};
+export const denver = {
+  name: "Denver Peaks",
+  logo: "../images/teams/denversiba.png",
+  alt: "Denver Peaks Team Logo"
+};
+export const detroit = {
+  name: "Detroit Legion",
+  logo: "../images/teams/detroitsiba.png",
+  alt: "Detroit Legion Team Logo"
+};
+export const houston = {
+  name: "Houston Stampede",
+  logo: "../images/teams/houstonsiba.png",
+  alt: "Houston Stampede Team Logo"
+};
+export const indiana = {
+  name: "Indiana Express",
+  logo: "../images/teams/indianasiba.png",
+  alt: "Indiana Express Team Logo"
+};
+export const kansasCity = {
+  name: "Kansas City Koyotes",
+  logo: "../images/teams/kansascitysiba.png",
+  alt: "Kansas City Koyotes Team Logo"
+};
+export const losAngeles = {
+  name: "Los Angeles Waves",
+  logo: "../images/teams/losangelessiba.png",
+  alt: "Los Angeles Waves Team Logo"
+};
+export const memphis = {
+  name: "Memphis Vandals",
+  logo: "../images/teams/memphissiba.png",
+  alt: "Memphis Vandals Team Logo"
+};
+export const miami = {
+  name: "Miami Sunbirds",
+  logo: "../images/teams/miamisiba.png",
+  alt: "Miami Sunbirds Team Logo"
+};
+export const milwaukee = {
+  name: "Milwaukee Wildcats",
+  logo: "../images/teams/milwaukeesiba.png",
+  alt: "Milwaukee Wildcats Team Logo"
+};
+export const minnesota = {
+  name: "Minnesota Lancers",
+  logo: "../images/teams/minnesotasiba.png",
+  alt: "Minnesota Lancers Team Logo"
+};
+export const montreal = {
+  name: "Montreal Argonauts",
+  logo: "../images/teams/montrealsiba.png",
+  alt: "Montreal Argonauts Team Logo"
+};
+export const newJersey = {
+  name: "New Jersey Hoops",
+  logo: "../images/teams/newjerseysiba.png",
+  alt: "New Jersey Hoops Team Logo"
+};
+export const newOrleans = {
+  name: "New Orleans Bears",
+  logo: "../images/teams/neworleanssiba.png",
+  alt: "New Orleans Bears Team Logo"
+};
+export const newYork = {
+  name: "New York Metros",
+  logo: "../images/teams/newyorksiba.png",
+  alt: "New York Metros Team Logo"
+};
+export const oklahomaCity = {
+  name: "Oklahoma City Twisters",
+  logo: "../images/teams/oklahomacitysiba.png",
+  alt: "Oklahoma City Twisters Team Logo"
+};
+export const philadelphia = {
+  name: "Philadelphia Silverbacks",
+  logo: "../images/teams/philadelphiasiba.png",
+  alt: "Philadelphia Silverbacks Team Logo"
+};
+export const phoenix = {
+  name: "Phoenix Scorpions ",
+  logo: "../images/teams/phoenixsiba.png",
+  alt: "Phoenix Scorpions Team Logo"
+};
+export const portland = {
+  name: "Portland Rhinos ",
+  logo: "../images/teams/portlandsiba.png",
+  alt: "Portland Rhinos Team Logo"
+};
+export const sanantonio = {
+  name: "San Antonio Monarchs",
+  logo: "../images/teams/sanantoniosiba.png",
+  alt: "San Antonio Monarchs Team Logo"
+};
+export const sandiego = {
+  name: "San Diego Captains",
+  logo: "../images/teams/sandiegosiba.png",
+  alt: "San Diego Captains Team Logo"
+};
+export const seattle = {
+  name: "Seattle Sasquatch",
+  logo: "../images/teams/seattlesiba.png",
+  alt: "Seattle Sasquatch Team Logo"
+};
+export const toronto = {
+  name: "Toronto Bears",
+  logo: "../images/teams/torontosiba.png",
+  alt: "Toronto Bears Team Logo"
+};
+export const vancouver = {
+  name: "Vancouver Rain",
+  logo: "../images/teams/vancouversiba.png",
+  alt: "Vancouver Rain Team Logo"
+};
+export const washington = {
+  name: "Washington Americans",
+  logo: "../images/teams/washingtonsiba.png",
+  alt: "Washington Americans Team Logo"
+};
