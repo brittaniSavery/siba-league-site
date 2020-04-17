50a2dcbdab8709dc3ff0187a22217b81693471de
Brittani S Avery <brittani.s.avery@gmail.com>
2020-04-17 01:26:40 -0500

    Finished retrieve and related tests

--- aws-functions/parse.js
+++ aws-functions/parse.js
@@ -1,6 +1,16 @@
 const fetch = require("node-fetch");
 
 exports.retrieve = async (body) => {
-  const response = await fetch(body);
-  return await response.text();
+  try {
+    const response = await fetch(body);
+    if (response.ok) {
+      const html = await response.text();
+      console.log(html);
+      return html;
+    } else {
+      throw new Error("Page could not be found.");
+    }
+  } catch (error) {
+    return error;
+  }
 };
--- aws-functions/test/test-parse.js
+++ aws-functions/test/test-parse.js
@@ -1,14 +1,27 @@
-const assert = require("chai").assert;
+const fs = require("fs");
+const chai = require("chai");
+const assert = chai.assert;
 const { retrieve } = require("../parse");
+
+//Quick helper function to load html files for testing/parsing
+require.extensions[(".html", ".htm")] = function (module, filename) {
+  exports = fs.readFileSync(filename, "utf8");
+};
+
+//Generated HTML files from the simulation basketball program
+var teamranking = require("../teamranking.htm");
 
 it("should retrieve webpage", async function () {
   const result = await retrieve(
     "http://localhost:3000/files/generated/SIBAStandings.html"
   );
-  assert.isNotNull(result, "SIBA Standings HTML body");
   assert.include(
     result,
-    "Simulation Internet Basketball Association Standings",
-    "Returns correct Standings text"
+    "Simulation Internet Basketball Association Standings"
   );
+});
+
+it("Should throw error for non-existent page", async function () {
+  const result = await retrieve("http://localhost:3000/doesNotExist.html");
+  assert.typeOf(result, "error");
 });
