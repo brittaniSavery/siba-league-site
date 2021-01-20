4754dfcc43a6a54f2f5c221abecb9e5fc3d9c27e
Brittani S Avery <brittani.s.avery@gmail.com>
2021-01-19 23:20:07 -0600

    Add dynamic height to iframe for generated htmls

--- frontend/src/components/IframeContainer.js
+++ frontend/src/components/IframeContainer.js
@@ -2,14 +2,26 @@ import React from "react";
 import Content from "../layout/Content";
 
 export default function IframeContainer({ file, header }) {
+  const [loading, setLoading] = React.useState(true);
+
+  const loadIframe = () => {
+    setLoading(false);
+    var frame = document.getElementById("generatedContent");
+    frame.style.height = frame.contentWindow.document.body.scrollHeight + "px";
+    frame.style.width = frame.contentWindow.document.body.scrollWidth + "px";
+    frame.style.visibility = "unset";
+  };
+
   return (
     <Content header={header}>
-      <div className="generatedContainer">
-        <iframe
-          title={header}
-          src={`${process.env.PUBLIC_URL}/files/${file}.html`}
-        />
-      </div>
+      {loading && <p className="text-center font-weight-bold">Loading...</p>}
+      <iframe
+        id="generatedContent"
+        title={header}
+        src={`${process.env.PUBLIC_URL}/files/${file}.html`}
+        onLoad={loadIframe}
+        style={{ visibility: "hidden" }}
+      />
     </Content>
   );
 }
